f105ad44a5bd9ce2fcb8a5ee6d50b0453a09d417
vincent <thanhvinhnguyenduc@gmail.com>
2023-12-01 18:05:50 +0700

    Parameterized CLI

--- app/orchestration/flows/cloud_deployment.py
+++ app/orchestration/flows/cloud_deployment.py
@@ -5,7 +5,7 @@ Serve a deployment to Prefect Cloud
 from prefect import flow
 from etl_gcs_to_bq import main_bq_flow
 from etl_web_to_gcs import main_gcs_flow
-from datetime import datetime
+from argparse import ArgumentParser
 
 
 @flow(
@@ -14,10 +14,9 @@ from datetime import datetime
     log_prints=True
 )
 def gcp_flow(
+    flowname: str,
     year: int,
-    month: int = 1,
-    days: list[int] = None,
-    hours: list[int] = None
+    month: int = 1
 ):
     """The Main Flow for Deployment:
     - Data Source to GCS
@@ -29,21 +28,37 @@ def gcp_flow(
         days (list[int], optional): a list of days. Defaults to (1 - 31)
         hours (list[int], optional): a list of hours. Defaults to (0 - 23)
     """
-    if days is None:
-        days = list(range(1, 32))
-    if hours is None:
-        hours = list(range(24))
-    main_gcs_flow(year, month, days, hours)
-    main_bq_flow(year, month, days, hours)
+    days = list(range(1, 32))
+    hours = list(range(24))
+
+    if flowname == "GCS":
+        main_gcs_flow(year, month, days, hours)
+    elif flowname == "BQ":
+        main_bq_flow(year, month, days, hours)
+    elif flowname == "ALL":
+        main_gcs_flow(year, month, days, hours)
+        main_bq_flow(year, month, days, hours)
+    else:
+        raise KeyError("This should not happen!")
 
 
 if __name__ == "__main__":
-    dt = datetime(2020, 1, 1)
+
+    parser = ArgumentParser(description="Deployment Configuration")
+    parser.add_argument("-f", "--flowname",
+                        choices=["GCS", "BQ", "ALL"], required=True, type=str)
+    parser.add_argument(
+        "-y", "--year", choices=list(range(2015, 2023)), default=2020, type=int)
+    parser.add_argument(
+        "-m", "--month", choices=list(range(1, 13)), default=1, type=int)
+    args = parser.parse_args()
+
+    flowname, year, month = args.flowname, args.year, args.month
     gcp_flow.serve(
-        name=f"GCP-Flow-Deployment-({dt.strftime('%b-%Y')})",
+        name=f"GCP-{flowname}-Deployment-({year - month})",
         parameters={
-            "year": dt.year,
-            "month": dt.month,
-            "days": list(range(2, 32))
+            "name": flowname,
+            "year": year,
+            "month": month
         }
     )
